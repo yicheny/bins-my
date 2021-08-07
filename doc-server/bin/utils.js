2b9da94acaf442fe39cbf4256dabf343ef85f58d
YLF <1463020554@qq.com>
2021-08-07 21:39:32 +0800

    init 初始化doc-server

---
+++ doc-server/bin/utils.js
@@ -0,0 +1,27 @@
+
+function createUtils(){
+    return {
+        isTrue(x){
+            return x === true;
+        },
+        isUndefined(x){
+            return x === undefined;
+        },
+        isString(x){
+            return typeof x === 'string'
+        },
+        isObject(x){
+            return this.getType(x) === 'Object';
+        },
+        isFunction(x){
+            return typeof x === 'function';
+        },
+        getType(value) {
+            let dataType = Object.prototype.toString.call(value);
+            dataType = dataType.slice(8, dataType.length - 1);
+            return dataType;
+        }
+    }
+}
+
+exports.utils = createUtils();
